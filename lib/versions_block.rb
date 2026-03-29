b54394aef0ffbf9e01ef76da7e4111cc6c9b2f23
Daniel Pepper <pepper.daniel@gmail.com>
2026-03-30 05:28:12 +0700

    .

--- lib/versions_block.rb
+++ lib/versions_block.rb
@@ -1,5 +1,17 @@
 require "versions_block/version"
 
 module VersionsBlock
+  extend self
 
+  def current_version
+    Thread.current[:versions_block_version]
+  end
+
+  def current_version=(version)
+    Thread.current[:versions_block_version] = version
+  end
+
+  def versions
+    []
+  end
 end
